18484b2309eec6bbd14b45998c789eb21cfd686a
yabdoul <95439560+yabdoul@users.noreply.github.com>
2024-05-23 13:22:32 +0000

    adding some modifs on food cards

--- src/components/foodcomp/index.js
+++ src/components/foodcomp/index.js
@@ -1,23 +1,46 @@
 import React from 'react'
 import "./styles.scss"
-import { UisStar } from '@iconscout/react-unicons-solid'
-export default function Index() {
+import { UilMapMarker } from '@iconscout/react-unicons'
+export default function Index(props) {
   return (
     <div className='food_comp'>
         <div className='food_top'>
-
             </div>
             <div className='food_bottom'>
-                <div className='rate-div'>
-                <UisStar color="orange" size="25" />
-                {"5  (52)"}
-                </div>
-                <div className='price-div'>
+               <div className='infos-div'> 
+                  <span>
+                    Bosco
+                  </span>
+                  <div className='loca-div'>
                     <span>
-                        71MAD/invite
-                        </span>
+                      2.5 KM
+                    </span>
+                    <UilMapMarker size="25" color="orange" />
+                  </div>
+              </div>
+              <div className='categ-div'>
+                 <span>
+                   Greece
+                  
+                  </span>
+                  {" . "}
+                  <span>
+                    Cocktail
+
+                    </span>
+                    {" . "}
+
+                    <span>
+                        Tajins
+
+                      </span> 
+                      {" . "}
+
+                      <span>
+                        Tajins
+                      </span> 
                 </div>
-            </div> 
+            </div>  
     </div>
   )
 }
